788e78bdea01f1d0811474744e94cec14b0ab1fe
Kshitij Lauria <kslauria@gmail.com>
2025-07-28 18:09:51 -0400

    auto format

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,12 @@
-export default function(eleventyConfig) {
-    eleventyConfig.setOutputDirectory("public");
-    eleventyConfig.addPassthroughCopy("bundle.css");
-    eleventyConfig.addPassthroughCopy("script.js");
-    eleventyConfig.addPassthroughCopy("_includes");
+export default function (eleventyConfig) {
+  eleventyConfig.setOutputDirectory("public");
+  eleventyConfig.addPassthroughCopy("bundle.css");
+  eleventyConfig.addPassthroughCopy("script.js");
+  eleventyConfig.addPassthroughCopy("_includes");
 
-    return {
-        dir: {
-          includes: "_includes"
-        }
-      }
-};
+  return {
+    dir: {
+      includes: "_includes",
+    },
+  };
+}
